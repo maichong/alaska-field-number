c7621bb21eef18f58b55824ca3c203da6f835d5c
Liang Xingchen <liang@maichong.it>
2016-04-22 01:43:42 +0800

    0.8.1

--- src/view.jsx
+++ src/view.jsx
@@ -119,7 +119,7 @@ export default class NumberFieldView extends React.Component {
 
     let label = field.nolabel ? '' : field.label;
 
-    if (field.fullWidth) {
+    if (field.horizontal === false) {
       let labelElement = label ? (
         <label className="control-label">{label}</label>
       ) : null;
